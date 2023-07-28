48564193cbe931a86838c8426f72d3c4bf0f934d
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2023-07-28 13:02:25 +0300

    Completed task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+const imagesMarkup = createImagesMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createImagesMarkup(images) {
+  return images
+    .map(({ preview, description, original }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt= "${description}"
+      loading="lazy"
+    />
+  </a>
+</li> `;
+    })
+    .join('');
+}
+
+let galleryBox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
