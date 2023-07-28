e4d6d730455bb6cd6e246370d9a2866107c50972
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2023-07-28 19:11:51 +0300

    added a solution to task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const data = {};
+
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+
+function onInput(e) {
+  data[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+}
